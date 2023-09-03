49b80d69b96538f69c8d866cfaeb7b25fff6edce
Sam <wwrechard@gmail.com>
2023-09-03 04:29:09 -0700

    update the UI components

--- tax_calculator/main.py
+++ tax_calculator/main.py
@@ -1,4 +1,5 @@
 import tax_calculator.constants as c
+import tax_calculator.tax_utils as utils
 
 import streamlit as st
 import numpy as np
@@ -7,33 +8,37 @@ import numpy as np
 st.title('Tax calculator')
 marital = st.selectbox('Martial status:', ['single', 'married'])
 state = st.selectbox('Resident state:', ['CA'])
+year = st.selectbox('Tax year:', [2023])
 
 # Inputs for the incomes
-st.header('Incomes')
-st.subheader('Your incomes:')
-income = st.number_input('Your base income ($):', step=c.AMOUNT_STEP)
-bonus = st.number_input('Your bonuses ($):', step=c.AMOUNT_STEP)
-rsu = st.number_input('Your RSUs ($):', step=c.AMOUNT_STEP)
-retirement_401k = st.number_input('Your pre-tax 401k contribution ($):', step=100)
+st.subheader('Incomes ($)')
+l, m, r = st.columns(3)
+with l:
+    income = st.number_input('Your base income:', step=c.AMOUNT_STEP)
+    bonus = st.number_input('Your bonuses:', step=c.AMOUNT_STEP)
+    rsu = st.number_input('Your RSUs:', step=c.AMOUNT_STEP)
+    retirement_401k = st.number_input('Your pre-tax 401k:', step=100, value=utils.cap_401k(year))
 
-st.subheader('Your spouse incomes (if applicalbe):')
-sp_income = st.number_input('Your spouse income ($):', step=c.AMOUNT_STEP)
-sp_bonus = st.number_input('Your spouse bonuses ($):', step=c.AMOUNT_STEP)
-sp_rsu = st.number_input('Your spouse RSUs ($):', step=c.AMOUNT_STEP)
-sp_retirement_401k = st.number_input('Your spouse pre-tax 401k contribution ($):', step=100)
+with m:
+    sp_income = st.number_input('Your spouse income:', step=c.AMOUNT_STEP)
+    sp_bonus = st.number_input('Your spouse bonuses:', step=c.AMOUNT_STEP)
+    sp_rsu = st.number_input('Your spouse RSUs:', step=c.AMOUNT_STEP)
+    sp_retirement_401k = st.number_input('Your spouse pre-tax 401k:', step=100, value=utils.cap_401k(year))
 
-st.subheader('Other income and deductables:')
-other_incomes = st.number_input('other incomes ($):')
-short_gain = st.number_input('short-term capital gains (loss) ($):', value=0)
-long_gain = st.number_input('long-term capital gains (loss) ($):', value=0)
-
-mortgage_interest = st.number_input('Primary home mortgage interests ($):')
-mortgage_amount = st.number_input('Primary home mortgage principal ($):')
-donations = st.number_input('Donations ($):', value=3000)
-salt = st.number_input('State tax withold + property tax ($):', value=10000)
+with r:
+    other_incomes = st.number_input('other incomes:')
+    short_gain = st.number_input('short-term capital gains (loss):', value=0)
+    long_gain = st.number_input('long-term capital gains (loss):', value=0)
 
 
-st.header('Federal Tax')
+st.subheader('Dedcutibes ($):')
+mortgage_interest = st.number_input('Primary home mortgage interests:')
+mortgage_amount = st.number_input('Primary home mortgage principal:')
+donations = st.number_input('Donations:', value=3000)
+salt = st.number_input('State tax withold + property tax:', value=10000)
 
 
-st.header('State Tax')
+st.subheader('Federal Tax')
+
+
+st.subheader('State Tax')
---
+++ tax_calculator/tax_utils.py
@@ -0,0 +1,3 @@
+def cap_401k(year):
+    if year == 2023:
+        return 22500
